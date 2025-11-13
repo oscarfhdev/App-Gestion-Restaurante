b6001ec7af33deb2182c05b25569bb05704e92d9
oscarfhdev <oscarfh.dev@gmail.com>
2025-11-13 22:11:18 +0100

    initialized project

---
+++ src/app_restaurante/controlador/PantallaLoginControlador.java
@@ -0,0 +1,16 @@
+package app_restaurante.controlador;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+
+public class PantallaLoginControlador implements Initializable {
+
+ 
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
