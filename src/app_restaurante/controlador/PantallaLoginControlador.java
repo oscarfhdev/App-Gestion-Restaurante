9ee0d00305b065c7243ae14531ca9a39c341d598
oscarfhdev <oscarfh.dev@gmail.com>
2025-11-16 01:22:00 +0100

    UPDATED src/app_restaurante/vista --> finished all UI login

--- src/app_restaurante/controlador/PantallaLoginControlador.java
+++ src/app_restaurante/controlador/PantallaLoginControlador.java
@@ -2,15 +2,39 @@ package app_restaurante.controlador;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 
 public class PantallaLoginControlador implements Initializable {
+
+    @FXML
+    private Button botonIniciarSesion;
+    @FXML
+    private Label labelErrorLogin;
+    @FXML
+    private TextField textFieldUsuario;
+    @FXML
+    private PasswordField textFieldContrasena;
 
  
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+    
+    @FXML
+    private void cerrarApp(MouseEvent event) {
+        System.exit(0);
+    }
+
+    @FXML
+    private void iniciarSesion(MouseEvent event) {
+    }
     
 }
