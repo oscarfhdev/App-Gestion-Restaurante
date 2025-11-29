c5ebbb83f61d71ce4acdbf0a328e1219ff7a7fd7
oscarfhdev <oscarfh.dev@gmail.com>
2025-11-29 20:50:06 +0100

    UPDATED src/app_restaurante/Gestion de Usuarios --> all UI designed

--- src/app_restaurante/controlador/PantallaGestionUsuariosControlador.java
+++ src/app_restaurante/controlador/PantallaGestionUsuariosControlador.java
@@ -1,5 +1,6 @@
 package app_restaurante.controlador;
 
+import java.io.IOException;
 import java.net.URL;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -7,19 +8,28 @@ import java.util.ResourceBundle;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 
 
 public class PantallaGestionUsuariosControlador implements Initializable {
 
     @FXML
-    private Label labelFechaHora;
+    private Label labelHora;
+    @FXML
+    private Label labelFecha;
+    @FXML
+    private ImageView iconoHome;
 
 
     @Override
@@ -29,17 +39,33 @@ public class PantallaGestionUsuariosControlador implements Initializable {
 
     
     public void inicializarFechayHora(){
-    // Formato de fecha y hora
-    DateTimeFormatter formatoHora = DateTimeFormatter.ofPattern("HH:mm:ss");
-    DateTimeFormatter formatoFecha = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        // Formato de fecha y hora
+        DateTimeFormatter formatoHora = DateTimeFormatter.ofPattern("HH:mm:ss");
+        DateTimeFormatter formatoFecha = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
-    // Evento que se ejecuta cada segundo
-    Timeline reloj = new Timeline(new KeyFrame(Duration.seconds(1), event -> {
-        LocalDateTime dateTimeActual = LocalDateTime.now();
-        labelFechaHora.setText(formatoFecha.format(dateTimeActual) + " " + formatoHora.format(dateTimeActual));
-    }));
-    reloj.setCycleCount(Timeline.INDEFINITE);
-    reloj.play();
+        // Evento que se ejecuta cada segundo
+        Timeline reloj = new Timeline(new KeyFrame(Duration.seconds(1), event -> {
+            LocalDateTime dateTimeActual = LocalDateTime.now();
+            labelHora.setText(formatoHora.format(dateTimeActual));
+            labelFecha.setText(formatoFecha.format(dateTimeActual));
+        }));
+        reloj.setCycleCount(Timeline.INDEFINITE);
+        reloj.play();
+    }
+
+    @FXML
+    private void irAPantallaPrincipal(MouseEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/app_restaurante/vista/pantallaPrincipal.fxml"));
+
+        Parent root = loader.load();
+        Scene escena = new Scene(root);
+        
+        // Obtener el Stage actual desde el botón
+        Stage stage = (Stage) iconoHome.getScene().getWindow();
+
+        // Reemplazar la escena actual
+        stage.setScene(escena);
+        stage.setTitle("Mesón Fernández - Inicio");  
     }
  
 }
